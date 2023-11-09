f28505b5700de0194d33404181c6fa9823a509a7
vince4559 <aigbojevincent6@gmail.com>
2023-11-09 03:10:06 +0100

    adding redux persist to auth

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,12 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
 import { apiSlice } from "./api/apiSlice"
 import authReducer from "../features/auth/authSlice"
+import {persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER
+} from 'redux-persist'
+
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const rootReducer = persistReducer(persistConfig, authReducer)
 
 export const store = configureStore({
     reducer:{
         [apiSlice.reducerPath]:apiSlice.reducer,
-        auth:authReducer
+        auth:rootReducer
     },
-    middleware:getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
+    middleware:getDefaultMiddleware => getDefaultMiddleware({
+        serializableCheck:{
+            ignoreActions:[FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+        }
+    }).concat(apiSlice.middleware),
     devTools: true,
-})
+});
+
+export let persistor = persistStore(store)
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,19 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './app/store.js'
+import { persistor, store } from './app/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-          <Routes>
-            <Route path='/*' element={<App />} />
-          </Routes>
-      </BrowserRouter>
+      <PersistGate persistor={persistor} loading={'loading...'}>
+        <BrowserRouter>
+            <Routes>
+              <Route path='/*' element={<App />} />
+            </Routes>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
